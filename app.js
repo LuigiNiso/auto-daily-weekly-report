f20a83fda44460a8b860236acb7807f7d6bc7fe8
Luigi Niso <65163646+LuigiNiso@users.noreply.github.com>
2023-09-04 10:11:43 +0200

    demo

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const file = new File(["foo"], "foo.txt", {
-    type: "text/plain",
-  });
+function demo() {
+    const file = new File(["foo"], "foo.txt", {
+        type: "text/plain",
+      });
+
+      console.log("Demo.");
+}
